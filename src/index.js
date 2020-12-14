8a9f716e096468e09b691b70cf23eeb12f110d08
Timendus <mail@timendus.com>
2020-12-15 00:05:39 +0100

    We can already warn about a calculator mismatch when the gets loaded

--- src/index.js
+++ src/index.js
@@ -109,6 +109,9 @@ function selectFile() {
       file = null;
       alert('Sorry!', 'The file you have selected does not seem to be a valid calculator file');
     }
+    if ( calculator && !calculator.canReceive(file) ) {
+      return alert('Careful!', `The file you have selected does not appear to be a valid file for your ${calculator.name}.`);
+    }
 
     updateButtons();
   });
